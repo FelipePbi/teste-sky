be8f498e091eadc9474a6011177a0f6f4aa41229
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-02-24 16:31:22 -0300

    feat: add Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+import Header from "../components/Header/Header";
+
+function Home() {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+}
+
+export default Home;
