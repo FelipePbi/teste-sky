9fd59ed1aaa3a4f27fa833323b5fc42dc8e3636a
Felipe Borges <felipe.rosa@grupoboticario.com.br>
2024-02-25 19:23:25 -0300

    feat: update global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/Home";
+import App from "./pages/Home/Home";
 import "./index.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-loading-skeleton/dist/skeleton.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
